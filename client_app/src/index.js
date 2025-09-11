809c2c771415c7a71550d1d2560d79eda4c9b1e7
Pham Khanh Huy <80874663+huytehuy@users.noreply.github.com>
2025-09-12 00:17:52 +0700

    updafe

--- client_app/src/index.js
+++ client_app/src/index.js
@@ -7,6 +7,10 @@ import store from './store';
 import { Provider } from 'react-redux';
 import './i18n/i18n'; // nhớ import file cấu hình i18next
 import './polyfills/node-polyfills';
+import 'process/browser';
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+window.process = process;
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
